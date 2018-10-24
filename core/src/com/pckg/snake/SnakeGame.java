6093319758a6862c0f6c33aac520e7b7ef04da6e
tesar27 <ytazhkeyev@gmail.com>
2018-10-24 10:03:48 +0200

    Initial commit

---
+++ core/src/com/pckg/snake/SnakeGame.java
@@ -0,0 +1,16 @@
+package com.pckg.snake;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SnakeGame extends Game {
+
+	@Override
+	public void create () {
+        setScreen(new GameScreen());
+	}
+
+}
